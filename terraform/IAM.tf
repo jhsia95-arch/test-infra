2077fe03b30bfc1279f7797a8c90022a78f18ee7
jiaheng <jhsia95@gmail.com>
2026-01-24 16:27:33 +0800

    update secrets rds

--- terraform/IAM.tf
+++ terraform/IAM.tf
@@ -1,14 +1,14 @@
-data "aws_eks_cluster" "this" {
-  name = var.cluster_name
-}
+# data "aws_eks_cluster" "this" {
+#   name = var.cluster_name
+# }
 
-data "aws_eks_cluster_auth" "this" {
-  name = var.cluster_name
-}
+# data "aws_eks_cluster_auth" "this" {
+#   name = var.cluster_name
+# }
 
-data "aws_iam_openid_connect_provider" "this" {
-  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
-}
+# data "aws_iam_openid_connect_provider" "this" {
+#   url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+# }
 locals {
   oidc_url = replace(data.aws_iam_openid_connect_provider.this.url, "https://", "")
 }
