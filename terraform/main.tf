a840a3ec2335708091576b12685e40046305a2ef
jiaheng <jhsia95@gmail.com>
2026-01-24 17:23:14 +0800

    update eks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = module.eks.cluster_auth_token  
+  token                  = module.eks.cluster_certificate_authority_data  
 }
 
 provider "aws" {
