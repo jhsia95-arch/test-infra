46b97fff1b2f9a6a9abc9ae2694f9fa270b86c58
jiaheng <jhsia95@gmail.com>
2026-01-24 17:40:35 +0800

    update eks

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -53,6 +53,9 @@ resource "helm_release" "secrets_store_csi" {
   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
   chart      = "secrets-store-csi-driver"
   namespace  = "kube-system"
+  
+  # This ensures Helm waits until the EKS control plane is actually up
+  depends_on = [module.eks] 
 
   set {
     name  = "syncSecret.enabled"
